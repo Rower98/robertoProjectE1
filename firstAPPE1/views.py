7608287ffc14b8cdc543005c065304c362f88f1d
Roberto espinoza <espinoza.roberto.98@hotmail.com>
2022-10-08 00:18:13 -0400

    cambios

--- firstAPPE1/views.py
+++ firstAPPE1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def display(request):
-    return HttpResponse("<h1>Hola mundo- Roberto Espinoza</h1>")
+
+def index(request):
+    return render(request,'index.html')
