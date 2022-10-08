9c093eb833d4cf8579e394a2667a6b306085eb5f
Roberto espinoza <espinoza.roberto.98@hotmail.com>
2022-10-08 00:27:08 -0400

    cambios

--- firstAPPE1/views.py
+++ firstAPPE1/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def userAdmin(request):
+    data = {
+        'isAdmin': True 
+    }
+    return render(request, 'index.html',data)
+
+def userView(request):
+    data = {
+        'isAdmin': False 
+    }
+    return render(request, 'index.html',data)       
--- robertoProjectE1/urls.py
+++ robertoProjectE1/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from firstAPPE1 import views
+from firstAPPE1.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.display)
+    path('',index)
 ]
