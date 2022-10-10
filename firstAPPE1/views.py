c0662410c06d8a71963ee521265356769cc3510a
Roberto espinoza <espinoza.roberto.98@hotmail.com>
2022-10-09 22:23:43 -0400

    proyectos

--- firstAPPE1/views.py
+++ firstAPPE1/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,'index.html')    
+    data = {
+        "nombre" : "Roberto",
+        "apellido" :"Espinoza",
+        "profesion" : "GUERREROOOO AU AU AU",
+        "edad" : "24",
+        "ciudad" : "talca",
+        "foto" : "/static/img/perfil.jpg",
+        "logo" : "/static/img/favicon.ico"
+    }
+    return render(request,'index.html',data)    
